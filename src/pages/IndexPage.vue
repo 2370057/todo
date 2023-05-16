3cdfba44ba355f5513097472ea6ce9f01ef1eeca
2370057 <rhdiddldmlcn@gmail.com>
2023-05-16 14:58:45 +0900

    commit first

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    >
+  </q-page>
+</template>
+
+<script>
+import { title } from 'process';
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'IndexPage',
+  title: "Vue를 재미있게 공부해봅시다!"
+})
+</script>
---
+++ src/pages/VueEx.vue
@@ -0,0 +1,34 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name:'',
+  title:"Vue Basic",
+  components:{},
+  data(){
+    return{
+
+    }
+  },
+  computed:{
+
+  },
+  watch:{
+
+  },
+  mounted(){
+
+  },
+  methods:{
+
+  }
+
+
+}
+</script>
+
+<style>
+
+</style>
